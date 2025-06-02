c299d38bcdbcf5f54e8c64a485f3655a1cab870c
Harsh-kub <harshkubade12345@gmail.com>
2025-06-02 20:37:44 +0530

    Create assignment 5.py

---
+++ assignment 5.py
@@ -0,0 +1,16 @@
+# assingment 5
+# task 1
+
+s={"alice":"85",
+   "harsh":"56",
+   "aheesh":"55",   }
+s1=input("Enter student's name : ")
+
+if s1 in s:
+    m=s[s1]
+    print(s1,"'s marks: ",m)
+else:
+    print("student not found")
+
+#task 2
+
