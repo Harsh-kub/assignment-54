1ac0ed59538d4e493cb7c15212da13a2df2d4691
Harsh-kub <harshkubade12345@gmail.com>
2025-06-04 10:31:38 +0530

    Update README.py

--- README.py
+++ README.py
@@ -14,4 +14,16 @@ else:
 
    # task 2:
 
+list1=[1,2,3,4,5,6,7,8,9,10] 
+list2=list1[0:5]
+print("Original  iist: ", list1)
+print("Extracted first five elements: ", list2)
+list2.reverse()
+print("Reversed extracted elements: ", list2 )list1=[1,2,3,4,5,6,7,8,9,10] 
+list2=list1[0:5]
+print("Original  iist: ", list1)
+print("Extracted first five elements: ", list2)
+list2.reverse()
+print("Reversed extracted elements: ", list2 )
+
    
